7fe8269b7933dbbaed3e2e7fc44af814d946e158
Abhishek88821 <jhaabhishek88821@gmail.com>
2023-12-20 00:45:56 +0530

    fix logo issues

--- src/app/Component/Header.js
+++ src/app/Component/Header.js
@@ -1,9 +1,7 @@
 import React from 'react'
-
 import Image from 'next/image';
 import Link from 'next/link'
-import logo from '../Logo.png';
-// import Menu from '../img/tabler_menu.svg';
+import Logo from '../Logo.png';
 import { FaEquals } from "react-icons/fa";
 
 
@@ -17,7 +15,7 @@ function Header() {
                         <div className="col-lg-6 col-md-6 col-sm-6 col-6">
                             <div className="log-div">
                                 <Link href="\" className="text-decoration-none" role="button">
-                                <Image src={logo} className="w-100 img-fluid"  alt="react logo" />
+                                <Image src={Logo} className="w-100 img-fluid"  alt="react logo" />
                                 </Link>
                             </div>
                         </div>
