917d8982ab9cc0b7b6dbf6a6d6f4b4e9141ba6f8
Abhishek88821 <127080867+Abhishek88821@users.noreply.github.com>
2023-12-22 03:01:22 +0530

    Update page.js

--- src/app/Form/page.js
+++ src/app/Form/page.js
@@ -21,14 +21,6 @@ import { LuMessageSquare } from "react-icons/lu";
 
 function page() {
 
-  
-const formSubmission = () => {
-
-}
-const handleChange = () => {
-
-}
-
 return (
     <>
         <section className="form-div pb-xl-5 pb-md-5 pb-5" >
@@ -40,13 +32,13 @@ return (
                         </div>
                         {/* form start */}
                         <div className="form-box" >
-                            <form className="form-div-box row p-0" method="" onSubmit={formSubmission()} >
+                            <form className="form-div-box row p-0" method="" >
                                 <div className='col-lg-6 col-md-6 col-sm-6 col-12' >
                                     <div className='frm-div-lbl' >
                                         <label>Full Name<span>*</span></label>
                                         <div className="frm-div-inp d-flex position-relative" >
                                             <LuUser2 className="frm-icn" />
-                                            <input type="text" className='form-control' placeholder='Type your good name' required="" name="fullName" onChange={handleChange} />
+                                            <input type="text" className='form-control' placeholder='Type your good name' required="" name="fullName"  />
                                             <LuCheck className="suc-sgn" />
                                         </div>
                                     </div>
@@ -56,7 +48,7 @@ return (
                                         <label>Phone Number<span>*</span></label>
                                         <div className="frm-div-inp d-flex position-relative" >
                                             <LuPhoneCall className="frm-icn" />
-                                            <input type="text" className='form-control' placeholder='0000000000' name="phoneNumber" onChange={handleChange} maxLength={10} required="" />
+                                            <input type="text" className='form-control' placeholder='0000000000' name="phoneNumber"  maxLength={10} required="" />
                                             <LuCheck className="suc-sgn" />
                                         </div>
                                     </div>
@@ -66,7 +58,7 @@ return (
                                         <label>Email Address<span>*</span></label>
                                         <div className="frm-div-inp d-flex position-relative" >
                                             <LuMail className="frm-icn" />
-                                            <input type="text" className='form-control' placeholder='example@email.com' name="emailAddress" onChange={handleChange} required="" />
+                                            <input type="text" className='form-control' placeholder='example@email.com' name="emailAddress"  required="" />
                                             <LuCheck className="suc-sgn" />
                                         </div>
                                     </div>
@@ -76,7 +68,7 @@ return (
                                         <label>City<span>*</span></label>
                                         <div className="frm-div-inp d-flex position-relative" >
                                             <LuMapPin className="frm-icn" />
-                                            <input type="text" className='form-control' placeholder='Type your city' required="" name="city" onChange={handleChange} />
+                                            <input type="text" className='form-control' placeholder='Type your city' required="" name="city"  />
                                             <BiSolidErrorCircle className="suc-sgn" />
                                         </div>
                                     </div>
@@ -86,7 +78,7 @@ return (
                                         <label>Type of Event<span>*</span></label>
                                         <div className="frm-div-inp d-flex position-relative" >
                                             <LuCalendarCheck className="frm-icn" />
-                                            <input type="text" className='form-control' placeholder='Choose event type' onChange={handleChange} name="typeofEvent" required="" />
+                                            <input type="text" className='form-control' placeholder='Choose event type'  name="typeofEvent" required="" />
                                             <LuChevronDown className="suc-sgn" />
                                         </div>
                                     </div>
