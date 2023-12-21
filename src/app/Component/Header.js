47016db8ed1261aa472d254bf6a0bef88768bea0
Abhishek88821 <127080867+Abhishek88821@users.noreply.github.com>
2023-12-21 22:04:13 +0530

    Update Header.js

--- src/app/Component/Header.js
+++ src/app/Component/Header.js
@@ -1,28 +1,39 @@
-import React from 'react'
+import React  from 'react'
 import Image from 'next/image';
 import Link from 'next/link'
-import Logo from '../Logo.png';
+import logo from '../Logo.png';
 import { FaEquals } from "react-icons/fa";
-
+import { BsSearch } from "react-icons/bs";
 
 function Header() {
+
     return (
         <>
             {/* <!-- header start --> */}
             <header className="hea-div">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-6 col-md-6 col-sm-6 col-6">
+                        <div className="col-lg-3 col-md-3 col-sm-4 col-6">
                             <div className="log-div">
                                 <Link href="\" className="text-decoration-none" role="button">
-                                <Image src={Logo} className="w-100 img-fluid"  alt="react logo" />
+                                    <Image src={logo} className="w-100 img-fluid" alt="react logo" />
                                 </Link>
                             </div>
                         </div>
-                        <div className="col-lg-6 col-md-6 col-sm-6 col-6">
+                        <div className="col-lg-7 col-md-8 col-sm-7 col-5" >
+                            <div className={"sea-div position-relative"}   >
+                                <form className="sea-frm-div" action="" method="" >
+                                    <div className="sea-frm-box" >
+                                        <BsSearch />
+                                        <input type="text" className="form-control" placeholder='Search Artists Here...' id="" />
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div className="col-lg-2 col-md-1 col-sm-1 col-1">
                             <div className="men-icn position-relative text-md-end">
                                 <Link href="javascript:void(0)" className="text-decoration-none" role="button">
-                                <FaEquals />
+                                    <FaEquals />
                                 </Link>
                             </div>
                         </div>
