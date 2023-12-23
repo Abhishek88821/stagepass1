6958109a943a9c46d3d6c34c2807ef5c43463fbb
Abhishek88821 <127080867+Abhishek88821@users.noreply.github.com>
2023-12-23 20:35:28 +0530

    Update stagepass.js

--- src/app/stagepass.js
+++ src/app/stagepass.js
@@ -1,8 +1,1 @@
-// $('.desc-course-cnt + a').click(function () {
-//     $('.desc-course-cnt').toggleClass('active');
-//     if ($('.desc-course-cnt + a')[0].innerText == 'read more') {
-//         $('.desc-course-cnt + a')[0].innerText = 'read less';
-//     } else {
-//         $('.desc-course-cnt + a')[0].innerText = 'read more';
-//     }
-// });
+
