ecdbb8df9614f9eeb7ec83499328af3e709e2c10
Abhishek88821 <127080867+Abhishek88821@users.noreply.github.com>
2023-12-28 02:21:33 +0530

    Add files via upload

--- src/app/Success/page.js
+++ src/app/Success/page.js
@@ -10,8 +10,8 @@ import Link from 'next/link';
 function page() {
     return (
         <>
-           {/* Logo start */}
-           <div className="hea-log" >
+            {/* Logo start */}
+            <div className="hea-log" >
                 <div className="container" >
                     <div className="row">
                         <div className="col-lg-12 col-md-12 col-12" >
@@ -32,8 +32,18 @@ function page() {
                             <div className="title  pb-0">
                                 <h1>Yaaay!</h1>
                                 <h2>Form Submitted Successfully</h2>
-                                <div class="success-img mx-auto" >
-                                    <Image src={success} className="img-fluid w-100" alt="Success Icon" />
+                                <div className="success-img mx-auto text-center" >
+                                    {/* <Image src={success} className="img-fluid w-100" alt="Success Icon" /> */}
+                                    <div className="animation-ctn">
+  	<div className="icon icon--order-success svg">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="154px" height="154px">  
+            <g fill="none" stroke="#046e7c" stroke-width="8"> 
+              <circle cx="77" cy="77" r="72" style={{strokeDasharray:'480px, 480px', strokeDashoffset: '960px'}}></circle>
+              {/* <circle id="colored" fill="transparent" cx="77" cy="77" r="72" style={{strokeDasharray:'480px, 480px', strokeDashoffset: '960px'}}></circle> */}
+              <polyline className="st0" stroke="#046e7c" stroke-width="12" points="43.5,77.8 63.7,97.9 112.2,49.4 " style={{strokeDasharray: '100px, 100px', strokeDashoffset: '200px'}}/>   
+            </g> 
+          </svg>
+          </div></div>
                                 </div>
                             </div>
                             {/* form start */}
